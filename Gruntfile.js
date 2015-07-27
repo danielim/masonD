bac5b87e243b425233ad7fa6f3fbde51cb1eca05
Daniel Chen <chen@daniel.im>
2015-07-27 17:14:27 -0400

    branched dev for more fluid dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
       },
       combinejs: {
         files: {
-          '<%= ghost_theme_name %>/assets/js/all.min.js': 
+          './<%= ghost_theme_name %>/assets/js/all.min.js': 
           [
           'bower_components/modernizr/modernizr.js',
           'custom_components/responsive_iframes/responsive_iframes.js'
@@ -54,11 +54,24 @@ module.exports = function(grunt) {
     cssmin: {
       combine: {
         files: {
-          '<%= ghost_theme_name %>/assets/css/style.css': ['bower_components/normalize-css/normalize.css', 'compiled_css/compiled_css.css']
+          './<%= ghost_theme_name %>/assets/css/style.css': ['bower_components/normalize-css/normalize.css', 'compiled_css/compiled_css.css']
         }
       }
     },
 
+    copy: {
+      main: {
+        files: [{
+          cwd: '<%= ghost_theme_name %>/',  // set working folder / root to copy
+          src: '**',           // copy all files and subfolders
+          dest: '<%= ghost_production_directory %>/content/themes/<%= ghost_theme_name %>/',    // destination folder
+          expand: true           // required when using cwd
+        }]
+      }
+    },
+    
+
+    'ghost_production_directory': '../../public/ghost',
     'ghost_theme_name': 'masonD',
 
   });
@@ -68,5 +81,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
 };
---
+++ ~/public/ghost/content/themes/assets/js/config.js
@@ -0,0 +1,2 @@
+var disqus_shortname = 'imdanielc';
+var disqus_identifier = '{{post.id}}';
